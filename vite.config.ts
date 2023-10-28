eac14eb622dd34641dfe35dc2dcec80551775956
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2023-10-28 09:09:22 -0700

    Add vite plugin & config to move/rename output files to static dir

--- vite.config.ts
+++ vite.config.ts
@@ -2,23 +2,41 @@ import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import tsconfigPaths from "vite-tsconfig-paths";
 import path from "path";
+import fs from "fs";
+
+const isP = process.env.NODE_ENV === "production";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [svelte(), tsconfigPaths()],
-    build: {
-        lib: {
-            entry: path.resolve(__dirname, "src/main.ts"),
-            name: "static/insound",
-            fileName: "static/insound",
-            formats: ["es"],
+    plugins: [svelte(), tsconfigPaths(), {
+        name: "dist-cleanup",
+        closeBundle: isP ? async () => {
+            // Remove hash from assets
+            const dir = path.resolve(__dirname, "dist/assets");
+            const newDir = path.resolve(__dirname, "dist/static")
+            const files = fs.readdirSync(dir);
+            files.forEach((file: string) => {
+              // Remove hash from file name
+              const newFile = file.replace(/-[a-z0-9]{8}\./, '.');
+              fs.renameSync(path.resolve(dir, file), path.resolve(newDir, newFile));
+            });
+          }
+        : undefined,
         },
+    ],
+    build: {
         rollupOptions: {
             output: {
-                assetFileNames: assetInfo => (assetInfo.name === "style.css") ?
-                    "static/style.css" : (assetInfo.name || ""),
+                assetFileNames: assetInfo => {
+                    if (assetInfo.name == "main.css") {
+                        return "static/main.css";
+                    }
+
+                    return assetInfo.name || "";
+                },
+
             },
-            external: ["svelte"],
+            input: "src/main.ts"
         },
         outDir: path.resolve(__dirname, "dist"),
     },
