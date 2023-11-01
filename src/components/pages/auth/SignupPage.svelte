0d9a99e74a1eb957e4e025d9cfb23855c6297a07
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2023-11-01 15:08:55 -0700

    Protect sign-up form

--- src/components/pages/auth/SignupPage.svelte
+++ src/components/pages/auth/SignupPage.svelte
@@ -37,6 +37,11 @@
             class="block w-full rounded-md border-0 px-1.5 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-purple-600 sm:text-sm sm:leading-6"/>
     </div>
 
+    <div class="hidden" aria-hidden="true">
+        <label for="username2">Please don't enter your username</label>
+        <input id="username2" type="text" name="username2" />
+    </div>
+
     <div class="text-center mx-auto mt-8">
         <input type="submit" value="Sign up" class="cursor-pointer text-sm w-full h-full py-2 text-gray-100 bg-purple-600 rounded-lg px-2"/>
     </div>
