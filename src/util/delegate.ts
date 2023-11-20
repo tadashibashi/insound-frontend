52cdcdf623f012a36c7e1a7c00088d0de232c68e
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2023-11-19 17:29:24 -0800

    Add handleCount and tryInvoke methods to Delegate

--- src/util/delegate.ts
+++ src/util/delegate.ts
@@ -40,6 +40,10 @@ export class Delegate<Ret, Args extends any[]> {
         this.isInvoking = false;
     }
 
+    get handleCount() {
+        return this.handles.length;
+    }
+
     subscribe(func: (...args: Args) => Ret, context?: any) {
         const newCallback = new Callback(func, context);
 
@@ -64,12 +68,24 @@ export class Delegate<Ret, Args extends any[]> {
         } else {
             this.removeCallback(toRemove);
         }
-
     }
 
-    invoke(...args: Args) {
+    /**
+     * Tries to invoke the handle, discarding return value and returning a
+     * boolean indicating whether invoke was successful. For simplicity,
+     * recursion is not allowed and will throw an error if attempted.
+     *
+     * @param  ...args - function arguments
+     *
+     * @return           whether invoke was successful.
+     *
+     * @throws any exception that may arise from subscribed callback functions,
+     *         or if this delegate is invoked inside of one of the callbacks.
+     */
+    tryInvoke(...args: Args): boolean {
         if (this.isInvoking)
             throw Error("Delegate cannot be invoked while already invoking.");
+        if (this.handles.length === 0) return false;
 
         this.isInvoking = true;
         for (let i = 0; i < this.handles.length; ++i) {
@@ -78,6 +94,42 @@ export class Delegate<Ret, Args extends any[]> {
         this.isInvoking = false;
 
         this.doCommands();
+
+        return true;
+    }
+
+    /**
+     * Invoke the callback, returning the first subscribed callback's return
+     * value. For simplicity, recursion is not allowed and will throw an error
+     * if attempted.
+     *
+     * @param   ...args   - arguments to pass to the callbacks
+     *
+     * @return  the first subscribed callback's return value
+     *
+     * @throws if there are no callback handles subscribed the the Delegate.
+     *         Please make sure to check `Delegate#handleCount` before calling.
+     *         Throws any other exception that may arise in callback functions,
+     *         or if this function is invoked inside one of the callbacks.
+     */
+    invoke(...args: Args): Ret {
+        if (this.isInvoking)
+            throw Error("Delegate cannot be invoked while already invoking.");
+        if (this.handles.length === 0)
+            throw Error("Cannot invoke Delegate without any handles");
+
+        this.isInvoking = true;
+        const length = this.handles.length;
+        const ret = this.handles[0].invoke(...args);
+        
+        for (let i = 1; i < length; ++i) {
+            this.handles[i].invoke(...args);
+        }
+        this.isInvoking = false;
+
+        this.doCommands();
+
+        return ret;
     }
 
     private doCommands() {
