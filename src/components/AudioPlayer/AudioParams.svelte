c9a578b5cbc1a3a49986d89da805ddedd2954849
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2023-12-06 02:44:52 -0800

    Add parameter/widget component stubs

---
+++ src/components/AudioPlayer/AudioParams.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    import { ParameterMgr } from "audio/params/ParameterMgr";
+
+    function onLoadParams(params: ParameterMgr)
+    {
+        
+    }
+</script>
---
+++ src/components/AudioPlayer/KnobWidget.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import { NumberParameter } from "app/audio/src/ts/params/types/NumberParameter";
+
+    export let param: NumberParameter;
+    $: value = param.value;
+
+</script>
+
+<div>
+   
+</div>
+
+<style>
+
+</style>
