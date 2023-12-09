e6643a3a5b05546d9ecfbe33575af265dd9267dd
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2023-12-09 13:13:59 -0800

    Add text shadow to labels

--- src/components/widgets/WidgetLabel.svelte
+++ src/components/widgets/WidgetLabel.svelte
@@ -2,4 +2,14 @@
     export let name: string;
 </script>
 
-<p class="text-center text-xs text-gray-200 mb-1 select-none">{name}</p>
+<p
+    aria-label="parameter name"
+    class="text-center text-xs text-gray-200 mb-1 select-none">
+    {name}
+</p>
+
+<style>
+    p {
+        text-shadow: 2px 2px 1px #00000008;
+    }
+</style>
