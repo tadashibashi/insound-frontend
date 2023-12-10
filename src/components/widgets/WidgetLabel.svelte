03b32d0352102478a4bd987872eb949c21cd1a29
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2023-12-10 11:22:26 -0800

    Update <WidgetLabel>: allow class override

--- src/components/widgets/WidgetLabel.svelte
+++ src/components/widgets/WidgetLabel.svelte
@@ -3,14 +3,14 @@
 </script>
 
 <label
-    aria-label="parameter name"
+    aria-label="parameter label"
     for={$$props["for"]}
-    class="block text-center text-xs text-gray-200 mb-1 select-none">
+    class={$$props["class"] || "WidgetLabel block text-center text-xs text-gray-200 mb-1 select-none"}>
     {name}
 </label>
 
 <style>
-    label {
+    label.WidgetLabel {
         text-shadow: 2px 2px 1px #00000008;
     }
 </style>
