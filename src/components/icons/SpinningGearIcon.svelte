4d2309fb49eab0c00ed3b517bbadb00d95b2903d
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2023-12-23 23:01:47 -0800

    Create SpinningGearIcon for loading buttons

---
+++ src/components/icons/SpinningGearIcon.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    import { Transition } from "@rgossiaux/svelte-headlessui";
+    import { Icon, Cog } from "svelte-hero-icons";
+
+    export let show: boolean = true;
+
+</script>
+
+<span class={($$props.class || "inline-block w-5 -translate-y-[1px]")}
+>
+    <!-- Fade and scale in/out -->
+    <Transition
+        {show}
+        enter="transition-opacity transition-transform duration-300"
+        enterFrom="opacity-0 scale-0 w-0"
+        enterTo="opacity-100 scale-100 w-full"
+        leave="transition-opacity transition-transform duration-300"
+        leaveFrom="opacity-100 scale-100 w-full"
+        leaveTo="opacity-0 scale-0 w-0"
+    >
+        <Icon class="animate-spin inline-block" src="{Cog}" />
+    </Transition>
+</span>
+
