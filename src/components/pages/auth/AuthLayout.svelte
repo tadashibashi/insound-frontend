a9c81e9f7bdd8f5eaed73173da424545828d89d5
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2023-12-25 15:56:30 -0800

    Add subtle fade-in to AuthLayout

--- src/components/pages/auth/AuthLayout.svelte
+++ src/components/pages/auth/AuthLayout.svelte
@@ -7,6 +7,7 @@
     import SignupPage from "./SignupPage.svelte";
     import VerificationConfirm from "./VerificationConfirm.svelte";
     import VerificationSentPage from "./VerificationSentPage.svelte";
+    import { Transition } from "@rgossiaux/svelte-headlessui";
 
     const subRoute = $$props["*"];
     const query = useQuery();
@@ -47,7 +48,15 @@
     });
 
 </script>
-
+<Transition
+    show={true}
+    enter="transition-opacity duration-300"
+    enterFrom="opacity-0"
+    enterTo="opacity-100"
+    leave="transition-opacity duration-300"
+    leaveFrom="opacity-100"
+    leaveTo="opacity-0"
+>
 <main>
     <button class="absolute right-4 top-4" on:click={() => exit()} tabindex="-1">
         <Icon src="{XMark}" class="h-8 w-auto text-gray-400" />
@@ -64,3 +73,4 @@
 
 
 </main>
+</Transition>
