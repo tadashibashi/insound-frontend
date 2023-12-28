33b4cda3e39d1343fb27780b12bb102a4e345b0e
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2023-12-28 02:37:31 -0800

    Update audio submodule to allow reverse min-max values in NumberParameter

---
+++ src/components/AudioEditor/AudioEditor.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    interface AudioEditorData {
+        audioFiles: {name: string, data: ArrayBuffer}[];
+        isLooping: boolean;
+        loopStart: number;
+        loopEnd: number;
+        markers: {name: string, offset: number}[];
+        presets: {name: string, volumes: number[]}[];
+        transitionTime: number;
+        script: string;
+    }
+</script>
