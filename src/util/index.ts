e9fbd972b09d12bbba337257328cd305cedadf2f
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2023-12-28 02:34:48 -0800

    Add fileNameToLabel utility function

--- src/util/index.ts
+++ src/util/index.ts
@@ -13,5 +13,63 @@ export namespace util
 
         return str;
     }
+
+    export function fileNameToLabelName(filename: string): string
+    {
+
+        function isSpace(c: string)
+        {
+            if (c.length === 0) return false;
+
+            switch(c.charAt(0))
+            {
+            case '-': case '_':
+                return true;
+            }
+
+            return /\s/.test(c);
+        }
+
+        // chop off file extension
+        filename = filename.replace(/\.[^/.]+$/, "");
+
+        let res = "";
+        let lastSpace = true;
+
+        // Use title case, transform _- to spaces, filter out all <>\
+        for (let i = 0; i < filename.length; ++i)
+        {
+            const c = filename[i];
+            if (lastSpace)
+            {
+                if (isSpace(c))
+                    continue;
+
+                if (/[A-Za-z]/.test(c))
+                {
+                    lastSpace = false;
+                    res += c.toUpperCase();
+                }
+                else
+                {
+                    res += c;
+                }
+            }
+            else
+            {
+                if (isSpace(c))
+                {
+                    lastSpace = true;
+                    res += ' ';
+                }
+                else
+                {
+                    res += c;
+                }
+            }
+        }
+
+        return res;
+    }
 }
 
