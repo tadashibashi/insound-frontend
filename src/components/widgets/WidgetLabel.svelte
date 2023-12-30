60dd1e26fd01cdba8e91188866ae86813c69acf7
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2023-12-29 19:39:56 -0800

    Add size to add style to WidgetLabel

--- src/components/widgets/WidgetLabel.svelte
+++ src/components/widgets/WidgetLabel.svelte
@@ -1,16 +1,17 @@
 <script lang="ts">
     export let name: string;
+    export let size: string = "";
 </script>
 
 <label
     aria-label="parameter label"
     for={$$props["for"]}
-    class={$$props["class"] || "WidgetLabel block text-center text-xs text-gray-200 mb-1 select-none"}>
+    class={$$props["class"] || "WidgetLabel block text-center text-gray-200 mb-1 select-none " + size}>
     {name}
 </label>
 
 <style>
     label.WidgetLabel {
-        text-shadow: 2px 2px 1px #00000008;
+        text-shadow: -1px -1px 1px #00000009;
     }
 </style>
