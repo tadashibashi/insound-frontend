ef7ad44746ca40ab3184bada12f074016c5bf3d8
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2023-12-29 19:38:10 -0800

    Use AudioChannel object inside of ChannelStrip, bundling logic and data

--- src/components/widgets/ChannelStrip.svelte
+++ src/components/widgets/ChannelStrip.svelte
@@ -1,68 +1,32 @@
 <script lang="ts">
-    import type { AudioEngine } from "app/audio/src/ts/AudioEngine";
     import VSlider from "./VSlider.svelte";
-    import { NumberParameter } from "audio/params/types/NumberParameter";
     import KnobWidget from "./KnobWidget.svelte";
+    import { AudioChannel } from "app/util/AudioChannel";
+    import WidgetLabel from "./WidgetLabel.svelte";
 
-    export let audio: AudioEngine;
-
-    /**
-     * 1-based, 0 means that it's the main channel
-     */
-    export let channel: number;
-
-    export let name: string;
-
-    let volume = new NumberParameter(name, channel, 0, 1.25, .01, 1, false,
-        (i, val) => {
-            if (channel > 0)
-                audio.setChannelVolume(channel-1, val);
-            else
-                audio.setMainVolume(val)
-        });
-
-    let reverb = new NumberParameter("rev", 0, 0, 2, .01, 0, false,
-        (i, val) => {
-            if (channel > 0)
-                audio.setChannelReverbLevel(channel-1, val);
-            else
-                audio.setMainReverbLevel(val);
-        });
-
-    let panLeft = new NumberParameter("pan l", 0, 100, 0, 1, 100, true,
-        (i, val) => {
-            if (channel > 0)
-                audio.engine.setChannelPanLeft(channel-1, val * .01);
-            else
-                audio.engine.setMainPanLeft(val);
-        });
-
-    let panRight = new NumberParameter("pan r", 0, 0, 100, 1, 100, true,
-        (i, val) => {
-            if (channel > 0)
-                audio.engine.setChannelPanRight(channel-1, val * .01);
-            else
-                audio.engine.setMainPanRight(val);
-        });
-
+    export let channel: AudioChannel;
 </script>
 
-<div class="flex flex-col">
-    <div class="w-8">
-        <KnobWidget param={reverb} />
-    </div>
-
-    <div class="flex">
-        <div class="w-6 mr-1">
-            <KnobWidget param={panLeft} showName={false} />
+<div class="w-18 inline-flex">
+    <div class="flex flex-col items-center">
+        <div class="w-8">
+            <KnobWidget param={channel.reverb} labelClass={"text-[10px]"} />
         </div>
 
-        <div class=w-6>
-            <KnobWidget param={panRight} showName={false} />
+        <div class="flex">
+            <div class="w-6 mr-1">
+                <KnobWidget param={channel.panLeft} showName={false} />
+            </div>
+
+            <div class=w-6>
+                <KnobWidget param={channel.panRight} showName={false} />
+            </div>
+        </div>
+
+        <VSlider param={channel.volume} height="100px" showName={false} />
+        <div class="overflow-visible">
+            <WidgetLabel name={channel.name} size="whitespace-normal " />
         </div>
     </div>
-
-
-    <VSlider param={volume} height="100px" />
 </div>
 
