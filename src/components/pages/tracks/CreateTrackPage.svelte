ff699a65669122539b4b5819316e91168fa7876a
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2024-01-01 02:07:13 -0800

    Refactor CreateTrackPage audio options into its own AudioOptionsPage

--- src/components/pages/tracks/CreateTrackPage.svelte
+++ src/components/pages/tracks/CreateTrackPage.svelte
@@ -1,22 +1,13 @@
 <script lang="ts">
-    import Form from "app/components/Form.svelte";
-    import { Result } from "app/util/api/Result";
     import {
-        ArrowDownTray, ArrowRight, ArrowSmallLeft, ArrowUpTray, EllipsisVertical, ExclamationCircle, Icon,
-        MusicalNote, PlusCircle, XCircle, XMark
+        ArrowSmallLeft, Icon
     } from "svelte-hero-icons";
-    import { Delegate } from "app/util/delegate";
-    import AudioPlayer from "app/components/AudioPlayer.svelte";
-    import TextEditor from "app/components/TextEditor/TextEditor.svelte";
 
     import { Transition } from "@rgossiaux/svelte-headlessui";
-    import type { MixPreset } from "app/util/MixPreset";
     import ErrorAlert from "app/components/widgets/ErrorAlert.svelte";
-    import Switch from "app/components/widgets/Switch.svelte";
-    import Modal from "app/components/Modal.svelte";
-    import { getContext } from "svelte";
     import { SoundLoadError } from "app/audio/src/ts/AudioEngine";
     import LoadAudioPage from "./LoadAudioPage.svelte";
+    import AudioOptionsPage from "./AudioOptionsPage.svelte";
 
     enum FormState {
         LoadFiles = 0,
@@ -33,47 +24,19 @@
     // 1: options + audio previewer
     let formState: FormState = FormState.LoadFiles;
 
-    let showAddMixModal = false;
-
-
-    // ===== Options ==========================================================
-
-    let mixPresets: MixPreset[] = [];
-    let showMarkers = true;
-    let looping = true;
-    let transitionTime = 1;
-
-    const defaultScript =
-`--Called after track finishes loading
-function on_ready()
-    print("Track is ready!")
-end
-
---Called when the playhead crosses a marker
-function on_marker(name, offset)
-    print("Marker: "..name..", "..offset)
-    if name == "LoopStart" then
-        print("Loop started")
-    end
-end
-`;
-
-    let audio = getContext("audio");
-
-    const onload = new Delegate<void, [ArrayBuffer | ArrayBuffer[], string[],
-        string]>;
-    const onunload = new Delegate<void, []>;
-    const onRequestText = new Delegate<string, []>;
-    const retrieveMix = new Delegate<MixPreset, []>;
+    // ===== Error messages ===================================================
 
     let errorTitle = "";
     let errorMessages: string[] = [];
     $: showErrors = errorMessages.length > 0;
 
+    let audioContext: AudioPlayerExternalControls;
+
+    // ===== Event handlers ===================================================
+
     async function onLoadAudio(files: File[])
     {
-        if (!files.length)
-            throw Error("No audio files");
+        if (files.length === 0) throw Error("No audio files");
 
         const promises: Promise<ArrayBuffer>[] = [];
         for (let i = 0; i < files.length; ++i)
@@ -81,38 +44,23 @@ end
             promises.push(files[i].arrayBuffer());
         }
 
+        let buffers: ArrayBuffer[];
         try {
-            const result = await Promise.all(promises)
-            loadAudio(new Result(result));
+            buffers = await Promise.all(promises)
         }
         catch(err)
         {
-            loadAudio(new Result([], err));
-        }
-    }
-
-    function loadAudio(payload: Result<ArrayBuffer[], unknown>)
-    {
-
-        if (!payload.ok)
-        {
-            errorMessages.push(payload.error?.toString() ||
-                "Application error: problem occurred while processing audio files.");
-            return false;
+            errorTitle = "An error occurred while processing files"
+            errorMessages.push("Conversion from File to ArrayBuffer failed.");
+            return;
         }
 
-        const buffers = payload.result;
-
-        let text = "";
         const names = fileInputs.map(input => input.layername);
-        names.pop(); // remove last one, since it's a dummy input waiting for user input
-        if (onRequestText.handleCount)
-        {
-            text = onRequestText.invoke();
-        }
+         // remove last one, since it's a dummy input waiting for user input
+        names.pop();
 
         try {
-            onload.invoke(buffers, names, text);
+            audioContext.load(buffers, names, "");
         }
         catch(err)
         {
@@ -133,7 +81,8 @@ end
             }
             else if (err instanceof Error)
             {
-                errorTitle = "An unexpected problem occured while loading the audio files";
+                errorTitle = "An unexpected error occured while loading the " +
+                    "audio files";
                 errorMessages.push(err.message);
             }
             errorMessages = errorMessages;
@@ -142,20 +91,9 @@ end
 
         // success, progress the form state
         formState = FormState.AudioOptions;
-        return true;
     }
 
 </script>
-
-<!-- Modals -->
-<Modal bind:show={showAddMixModal} isCancellable={true}>
-    <div class="w-full h-full fixed flex items-center justify-center">
-        <div class="bg-white rounded-md w-1/2 min-w-[200px] h-auto">
-            Hello
-            Lorem Ipsum
-        </div>
-    </div>
-</Modal>
 
 <!-- Header -->
 <h1 class="ml-10 mt-2 text-3xl select-none">New Track</h1>
@@ -170,108 +108,34 @@ end
     leave="transition-opacity duration-300"
     leaveFrom="opacity-100"
     leaveTo="opacity-0"
-
     on:outroend={() => errorMessages.length = 0}
 >
     <ErrorAlert title={errorTitle} errorList={errorMessages} oncancel={() => showErrors = false}/>
 </Transition>
 
-<LoadAudioPage bind:fileInputs={fileInputs} onsubmit={onLoadAudio} show={formState === FormState.LoadFiles} />
+<!-- Back Button -->
+{#if formState > 0}
+<button class="absolute left-2 flex-none rounded-full hover:bg-gray-100 px-3 py-1 transition-colors duration-300"
+    on:click={() => {
+        if (formState === FormState.AudioOptions)
+        {
+            audioContext.unload();
+        }
 
-<!-- FormState.AudioOptions -->
-<div class={"absolute w-full mt-4 transition-opacity duration-300 flex justify-center "
-    + (formState === 1 ? "opacity-100" : "opacity-0 pointer-events-none")}>
+        --formState;
+    }}
+>
+    <Icon class="inline" size="14" src="{ArrowSmallLeft}" /> Back
+</button>
+{/if}
 
+<LoadAudioPage
+    show={formState === FormState.LoadFiles}
+    onsubmit={onLoadAudio}
+    bind:fileInputs={fileInputs} />
 
-
-    <!-- Back Button -->
-    <button class="absolute left-2 flex-none rounded-full hover:bg-gray-100 px-3 py-1 transition-colors duration-300"
-        on:click={() => {
-            onunload.tryInvoke();
-            formState = FormState.LoadFiles;
-        }}
-    >
-        <Icon class="inline" size="14" src="{ArrowSmallLeft}" /> Back
-    </button>
-
-    <!-- AudioOptions main container -->
-    <div class="w-3/4">
-
-        <!-- Options -->
-        <table>
-            <tbody>
-                <tr>
-                     <!-- Show Markers -->
-                    <td class="p-1">
-                        <label for="show-markers-input" class="block text-xs font-bold select-none">
-                            Show Markers
-                        </label>
-                    </td>
-                    <td>
-                        <Switch id="show-markers-input" bind:enabled={showMarkers} description="Option whether to show audio markers to end viewer." />
-                    </td>
-
-                    <!-- Looping -->
-                    <td class="p-1">
-                        <label for="is-looping-input" class="ml-4 block text-xs font-bold select-none">
-                            Looping
-                        </label>
-                    </td>
-                    <td>
-                        <Switch id="is-looping-input" bind:enabled={looping} description="Option whether to set track to looping"/>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
-
-        <!-- Player -->
-        {#if $audio}
-        <AudioPlayer
-            audio={$audio}
-            onload={onload} onunload={onunload}
-            mixPresets={mixPresets}
-            showMarkers={showMarkers}
-            looping={looping}
-            transitionTime={transitionTime}
-            retrieveMix={retrieveMix}
-        />
-        {/if}
-
-        <!-- Mix Presets -->
-        <div class="flex justify-between">
-            <!-- Title -->
-            <div class="flex">
-                <h2 class="text-xl mr-4">Mix Presets</h2>
-                <button on:click={() => {
-                    showAddMixModal = true;
-                }}
-                >Save Mix <Icon class="inline" src="{PlusCircle}" size="16" /></button>
-            </div>
-
-            <!-- Transition Time -->
-            <tr>
-                <td class="p-1">
-                    <label for="transition-time" class="block text-xs font-bold">
-                        Transition Time (s)
-                    </label>
-                </td>
-                <td>
-                    <input id="transition-time" type="number" min="0" max="10" step=".1" bind:value={transitionTime} class="pl-2 border border-gray-100"/>
-                </td>
-            </tr>
-        </div>
-
-        <!-- Script -->
-        <h2 class="text-xl mb-3 ml-2">Script</h2>
-        <TextEditor
-            onRequestText={onRequestText}
-            onSave={()=> console.log("saved.")}
-            value={defaultScript}
-        />
-
-        <!-- Submit Button -->
-        <button type="button" class="block mx-auto mt-4 px-2 py-1 bg-violet-700 text-white rounded-md">Submit</button>
-    </div>
-
-</div>
+<AudioOptionsPage
+    show={formState === FormState.AudioOptions}
+    onsubmit={ (data) => { console.log("submitted data:", data);} }
+    bind:audioContext={audioContext} />
 
