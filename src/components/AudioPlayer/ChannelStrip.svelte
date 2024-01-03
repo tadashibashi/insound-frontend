52afe12a73fff6dabfe55024a4b3c150407520c2
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2024-01-03 01:35:47 -0800

    Update MixConsole - add shadow to indicate scroll-ability

--- src/components/AudioPlayer/ChannelStrip.svelte
+++ src/components/AudioPlayer/ChannelStrip.svelte
@@ -7,12 +7,15 @@
     export let channel: AudioChannel;
 </script>
 
-<div class="w-[124px] inline-flex">
+<div class={$$props.class ?? "w-[124px] inline-flex pt-3 "}>
     <div class="flex flex-col items-center w-full">
+
+        <!-- Reverb Knob -->
         <div class="w-8">
             <KnobWidget param={channel.reverb} labelClass={"text-[10px]"} />
         </div>
 
+        <!-- Pan -->
         <div class="flex">
             <div class="w-6 mr-1">
                 <KnobWidget param={channel.panLeft} showName={false} />
@@ -23,13 +26,11 @@
             </div>
         </div>
 
-
+        <!-- Volume Slider -->
         <VSlider param={channel.volume} height="100px" showName={false} />
         <div class="overflow-visible mx-auto">
             <WidgetLabel name={channel.name} size="whitespace-normal block" />
         </div>
-
-
     </div>
 </div>
 
--- src/components/AudioPlayer/MixConsole.svelte
+++ src/components/AudioPlayer/MixConsole.svelte
@@ -1,42 +1,83 @@
 <!--
 @file MixConsole.svelte
 @description
-    Interface to control underlying AudioEngine with the appearance of an
+    Interface to control AudioConsole parameters, with the appearance of an
     audio mixing console desk.
 -->
 <script lang="ts">
     import { AudioConsole } from "app/util/AudioConsole";
     import ChannelStrip from "./ChannelStrip.svelte";
+    import { onMount } from "svelte";
 
     /** Audio console to control */
     export let audioConsole: AudioConsole;
 
+    let channelDiv: HTMLDivElement;
+    let showMainShadow: boolean = false;
+    let showLeftShadow: boolean = false;
+
+    function checkShadows()
+    {
+        const scrollbarVisible = checkHScrollbarVisible(channelDiv);
+        showMainShadow = scrollbarVisible &&
+            channelDiv.scrollLeft < channelDiv.scrollWidth -
+            channelDiv.clientWidth - 1;
+        showLeftShadow = scrollbarVisible && channelDiv.scrollLeft !== 0;
+    }
+
+    function checkHScrollbarVisible(elem: HTMLElement)
+    {
+        return elem.clientWidth < elem.scrollWidth;
+    }
+
+    onMount(() => {
+        window.addEventListener("resize", checkShadows);
+
+        checkShadows();
+        return () => {
+            window.removeEventListener("resize", checkShadows);
+        };
+    });
+
 </script>
 
 <!-- Mix Console -->
-<!-- outer container -->
-<div class="border border-b-2 border-gray-50 rounded-md my-6 shadow-lg">
-    <!-- inner container -->
-    <div class="h-[324px] w-full flex pt-2 pl-2">
+<div class="border-l-2 border-l-white border-t-2 border-t-white border-r-2 border-r-gray-50 border-b-2 border-b-gray-50 h-[324px] w-full flex overflow-hidden relative bg-[#fefefe] rounded-md shadow-md">
 
-        <!-- Channel strips -->
-        <div class="overflow-y-hidden overflow-x-auto whitespace-nowrap flex-grow flex">
-            {#each audioConsole.channels as chan, i (chan)}
-                {#if i > 0}
-                    <ChannelStrip channel={chan} />
+    <!-- left shadow -->
+    <div class={"z-50 absolute w-2 h-full -translate-x-2 " + (showLeftShadow ? "ChanShadowRight" : "")}></div>
 
-                    <!-- Channel divider -->
-                    <div class="inline h-[300px] w-[1px] border-l border-l-gray-100"></div>
-                {/if}
-            {/each}
+    <!-- Channel strips -->
+    <div bind:this={channelDiv}
+        on:scroll={checkShadows}
+        class="overflow-y-hidden overflow-x-auto whitespace-nowrap flex-grow flex">
+        {#each audioConsole.channels as chan, i (chan)}
+            {#if i > 0}
+                <ChannelStrip channel={chan} />
 
-        </div>
-
-        <!-- Main channel bus -->
-        {#if audioConsole.channels.length > 0}
-            <ChannelStrip channel={audioConsole.channels[0]} />
-        {/if}
+                <!-- Channel divider -->
+                <div class="inline border-l border-l-gray-100 my-4"></div>
+            {/if}
+        {/each}
 
     </div>
 
+    <!-- Main channel bus -->
+    {#if audioConsole.channels.length > 0}
+        <div class={"inline-flex w-[124px] pt-3 flex justify-center " + (showMainShadow ? "ChanShadowLeft" : "")}>
+            <ChannelStrip class="" channel={audioConsole.channels[0]} />
+        </div>
+
+    {/if}
+
 </div>
+
+
+<style>
+    .ChanShadowLeft {
+        box-shadow: -4px 0 8px #0000000f;
+    }
+    .ChanShadowRight {
+        box-shadow: 4px 0 16px #0000000f;
+    }
+</style>
