c43bcad8fb7d4a23ee9085d50ff8a4a93ae8c3e2
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2024-01-07 05:35:09 -0800

    Fix placement of gear icon (may replace it, it looks weird)

--- src/components/icons/SpinningGearIcon.svelte
+++ src/components/icons/SpinningGearIcon.svelte
@@ -6,17 +6,19 @@
 
 </script>
 
-<span class={($$props.class || "inline-block w-4 -translate-y-[1px]")}
+<span class={($$props.class || "inline-block w-4 h-full")}
 >
     <!-- Fade and scale in/out -->
     <Transition
         {show}
+        as="div"
         enter="transition-opacity transition-transform duration-300"
         enterFrom="opacity-0 scale-0 w-0"
         enterTo="opacity-100 scale-100 w-full"
         leave="transition-opacity transition-transform duration-300"
         leaveFrom="opacity-100 scale-100 w-full"
         leaveTo="opacity-0 scale-0 w-0"
+        class="flex justify-center items-center"
     >
         <Icon class="animate-spin inline-block" src="{Cog}" mini />
     </Transition>
