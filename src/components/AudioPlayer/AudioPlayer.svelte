21a888d30625b0c9118e501140f0a45e6ab548b9
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2024-01-13 15:05:25 -0800

    Move add mix modal into its own component

--- src/components/AudioPlayer/AudioPlayer.svelte
+++ src/components/AudioPlayer/AudioPlayer.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-    import type { AudioChannelSettings } from "audio/AudioChannel";
     import { AudioConsole } from "audio/AudioConsole";
     import type { AudioEngine } from "audio/AudioEngine";
     import type { MixPreset } from "audio/MixPresetMgr";
@@ -8,7 +7,7 @@
 
     import { onMount } from "svelte";
 
-    import { AdjustmentsVertical, Icon, Pause, Play, PlusCircle, XMark } from "svelte-hero-icons";
+    import { Icon, Pause, Play } from "svelte-hero-icons";
 
     import Playbar from "./Playbar.svelte";
     import MixConsole from "./MixConsole.svelte";
@@ -16,14 +15,12 @@
     import WaveformMorphDisplay from "./WaveformMorphDisplay.svelte";
     import { WaveMorpher } from "app/util/WaveMorpher";
     import { MultiTrackControl } from "audio/MultiTrackControl";
-    import Modal from "../Modal.svelte";
     import MixList from "./MixList.svelte";
     import VolumeSlider from "./VolumeSlider.svelte";
 
     export const context: AudioPlayerExternalControls = {
         load: onLoadAudio,
         unload: onUnloadAudio,
-        getCurrentMix: getCurrentMix,
     };
 
     // Providing the AudioEngine externally ensures it always exists in this
@@ -49,18 +46,13 @@
 
     let loopend: number = 0;
 
-    let showAddMixModal = false;
-    let mixName = "";
+    let presetChoice: MixPreset;
 
     export let showMarkers: boolean = true;
     export let looping: boolean = true;
     export let transitionTime: number = 1;
 
     export let mixPresets: MixPreset[] = [];
-
-    // todo: implement param page later
-    // export let params: Param[] = [];
-
 
     $: if (track.isLoaded)
     {
@@ -125,6 +117,7 @@
         audioConsole.addChannels(["Main", ...pLayerNames]);
         audioConsole = audioConsole;
 
+        console.log("There are " + track.channelCount + " channels");
         wave.loadData(track, audioConsole);
     }
 
@@ -180,34 +173,11 @@
         isPlaying = !newPause;
     }
 
-    // ----- Helpers ----------------------------------------------------------
 
-    // Helper to apply a mix setting to the audio console
-    function applyMix(mix: AudioChannelSettings[], transitionTime: number = 0)
-    {
-        if (!track.isLoaded) return;
-
-        audioConsole.applySettings(mix, transitionTime);
-    }
-
-    // Helper to get current mix from the audio console
-    function getCurrentMix(name?: string)
-    {
-        return {name: name || "", mix: audioConsole.getCurrentSettings()};
-    }
-
-    function saveMix()
-    {
-        if (mixName.length === 0) return;
-
-        mixPresets.push(getCurrentMix(mixName));
-        mixPresets = mixPresets;
-        showAddMixModal = false;
-    }
 </script>
 
 <!-- Player Container -->
-<div class="relative select-none">
+<div class="relative select-none min-w-[340px]">
 
     <Playbar
         class="relative w-full z-10 shadow-md rounded-t-md h-[100px] bg-gray-400"
@@ -228,87 +198,46 @@
 
 
     <!-- Play controls bar -->
-    <div class="relative w-full h-10 flex items-center bg-gray-400 text-gray-200 shadow-md mt-1">
-        <!-- Play/Pause Button -->
-        <button
-            class="px-3 h-6 w-6 rounded-md box-content z-50 relative text-gray-200 hover:text-gray-100 transition-transform duration-300"
-            on:click={onPressPlay}
-            >
-            {#if isPlaying}
-                <Icon class="drop-shadow-sm z-50" solid src="{Pause}" />
-            {:else}
-                <Icon class="drop-shadow-sm z-50" solid src="{Play}" />
-            {/if}
-        </button>
+    <div class="relative w-full h-10 flex justify-between items-center bg-gray-400 text-gray-200 shadow-md mt-1">
 
-        <!-- TODO: use local storage to save and grab this value -->
-        <VolumeSlider initVolume={1} onchange={(val) => audio.masterVolume = val} />
+        <!-- Left side of bar -->
+        <div class="flex items-center">
 
-        <!-- Time -->
-        <div class="text-xs font-bold">
-            <p class="text-gray-200">
-                <span>{time.toString()}</span>
-                <span> / </span>
-                <span>{time.toString(time.max)}</span>
-            </p>
-        </div>
-
-        <div class="flex justify-center items-center z-50 text-white absolute w-full pointer-events-none">
-            {#if mixPresets.length > 0}
-            <Icon size="20" class="inline-block me-1" src={AdjustmentsVertical} />
-            <MixList presets={mixPresets}
-                onchoice={(preset, index) => console.log(preset.name, index)}
-                ondrop={(from, to) => {
-                    console.log(from, to);
-                    const temp = [...mixPresets];
-                    const preset = temp[from];
-                    temp.splice(from, 1);
-                    temp.splice(to, 0, preset);
-                    mixPresets = temp;
-                }}
-            />
-            {/if}
-        </div>
-
-        <div class="flex justify-end items-center z-50 text-white absolute w-full pointer-events-none">
-            <button class="pointer-events-auto" on:click={() => showAddMixModal = true}>
-                <Icon src={PlusCircle} size="16" />
+            <!-- Play/Pause Button -->
+            <button
+                class="px-3 h-6 w-6 rounded-md box-content z-50 relative text-gray-200 hover:text-gray-100 transition-transform duration-300"
+                on:click={onPressPlay}
+                >
+                {#if isPlaying}
+                    <Icon class="drop-shadow-sm z-50" solid src="{Pause}" />
+                {:else}
+                    <Icon class="drop-shadow-sm z-50" solid src="{Play}" />
+                {/if}
             </button>
+
+            <!-- Volume controls -->
+            <!-- TODO: use local storage to save and grab this value -->
+            <VolumeSlider initVolume={1} onchange={(val) => audio.masterVolume = val} />
+
+            <!-- Time -->
+            <div class="text-[10px] sm:text-xs font-bold">
+                <p class="text-gray-200">
+                    <span>{time.toString()}</span>
+                    <span> / </span>
+                    <span>{time.toString(time.max)}</span>
+                </p>
+            </div>
         </div>
 
-
+        <!-- Right side of bar -->
+        <MixList
+            bind:presets={mixPresets}
+            bind:choice={presetChoice}
+            mixConsole={audioConsole}
+            transitionTime={transitionTime}
+        />
     </div>
-
 
     <MixConsole audioConsole={audioConsole} />
-    <!-- Mix preset options -->
-<!--     <ChoiceMenu class="" choices={mixPresets.map(preset => preset.name)}
-        onchoose={value => applyMix(mixPresets[value].mix, transitionTime)} /> -->
-
 </div>
 
-<!-- Add mix modal -->
-<Modal bind:show={showAddMixModal} isCancellable={true} onopen={() => mixName = ""}>
-    <div class="w-full h-full fixed flex items-center justify-center">
-
-        <div class="relative bg-white rounded-md w-1/2 min-w-[200px] h-auto pt-4 pb-4">
-            <!-- cancel button -->
-            <button class="absolute top-1 right-1 rounded-full hover:bg-gray-100 transition-colors duration-300 text-gray-400 p-1" on:click={() => showAddMixModal = false }>
-                <Icon src={XMark} size="24" />
-            </button>
-
-            <Icon class="block text-center mx-auto text-gray-400 mb-1" src={AdjustmentsVertical} size="48" />
-            <p class="text-center text-lg mb-1">Save Mix Preset</p>
-            <label class="w-full block ml-4">
-                <p class="inline mr-2 text-gray-400">Name</p>
-                <input class="px-2 border border-gray-100" type="text" bind:value={mixName} minlength="1" />
-            </label>
-
-            <button
-                on:click={saveMix}
-                class={"block mx-auto rounded-full mt-4 mb-2 px-4 py-2 " + (mixName.length > 0 ? "bg-violet-400 text-white cursor-pointer" : "bg-gray-100 text-gray-50 cursor-not-allowed")}>
-                Add Mix
-            </button>
-        </div>
-    </div>
-</Modal>
---
+++ src/components/AudioPlayer/modals/AddMixModal.svelte
@@ -0,0 +1,71 @@
+<script lang="ts">
+    import Modal from "app/components/Modal.svelte";
+    import { AdjustmentsVertical, Icon, XMark } from "svelte-hero-icons";
+
+    export let onsubmit: ((name: string, copyCurrent: boolean) => void) | undefined = undefined;
+    export let show = true;
+
+    let name: string = "";
+
+    function handleSubmit(evt: SubmitEvent)
+    {
+        evt.preventDefault();
+        
+        const checked = document.querySelector("input[name='mix-source']:checked") as HTMLInputElement;
+        if (!checked)
+            throw Error("Could not find mix-source checked element");
+
+        if (onsubmit)
+        {
+            onsubmit(name, (checked.value === "current"));
+            show = false;
+        }
+    }
+</script>
+
+
+<Modal bind:show={show} isCancellable={true} onopen={() => name = ""}>
+    <div class="w-full h-full fixed flex items-center justify-center">
+
+        <form class="relative bg-white rounded-md w-1/2 min-w-[200px] h-auto pt-4 pb-4" on:submit={handleSubmit}>
+            <!-- cancel button -->
+            <button
+                class="absolute top-1 right-1 rounded-full hover:bg-gray-100 transition-colors duration-300 text-gray-400 p-1"
+                on:click={() => show = false }
+            >
+                <Icon src={XMark} size="24" />
+            </button>
+
+            <!-- Mix icon -->
+            <Icon class="block text-center mx-auto text-gray-400" src={AdjustmentsVertical} size="36" />
+
+            <!-- Title -->
+            <p class="text-center text text-gray-500 mb-3">New Mix Preset</p>
+
+            <!-- Name text input -->
+            <label class="w-full flex justify-center text-sm">
+                <p class="inline mr-2 text-gray-400">Name</p>
+                <input class="px-2 border border-gray-100 rounded-sm" type="text" bind:value={name} minlength="1" />
+            </label>
+
+            <!-- Mix source radio input -->
+            <div class="text-sm w-auto translate-x-[calc(50%-68px)]">
+                <label class="block">
+                    <input type="radio" name="mix-source" checked value="current" />
+                    <p class="inline mr-2 text-gray-400">Copy current settings</p>
+                </label>
+
+                <label class="block">
+                    <input type="radio" name="mix-source" value="default" />
+                    <p class="inline mr-2 text-gray-400">New default mix</p>
+                </label>
+            </div>
+
+            <button
+                on:click={handleSubmit}
+                class={"block mx-auto rounded-full mt-4 mb-2 px-2 py-1 text-sm " + (name.length > 0 ? "bg-violet-400 text-white cursor-pointer" : "bg-gray-100 text-gray-50 cursor-not-allowed")}>
+                Add Mix
+            </button>
+        </form>
+    </div>
+</Modal>
