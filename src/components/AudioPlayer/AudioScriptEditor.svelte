1206d537b726ca2a34c9c3545f6c95f068516acf
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2024-01-24 16:24:47 -0800

    Add non-editMode feature and styles to AudioScriptEditor - no interaction and read-only status

--- src/components/AudioPlayer/AudioScriptEditor.svelte
+++ src/components/AudioPlayer/AudioScriptEditor.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
     import { TextEditorMgr } from "app/util/TextEditorMgr";
     import { onMount } from "svelte";
-    import { ArrowUturnLeft, ArrowUturnRight, Icon } from "svelte-hero-icons";
+    import { ArrowUturnLeft, ArrowUturnRight, Icon, LockClosed } from "svelte-hero-icons";
+
+    export let editMode: boolean = true;
 
     // Bindable text editor object, or you can provide your own
     export let editor: TextEditorMgr;
@@ -42,12 +44,23 @@
         // Create the text editor object
         if (!editor)
         {
-            editor = new TextEditorMgr({text: value});
+            editor = new TextEditorMgr({text: value, editMode});
             editorParentEl.appendChild(editor.parent);
         }
 
         // Get the editor element that contains scrolling
         scrollerEl = editor.scrollDOM;
+
+        // attach screen to block mouse interaction with top
+        if (!editMode)
+        {
+            const screenEl = document.createElement("div");
+            screenEl.className = "fixed inset-0 cursor-default bg-gray-50 opacity-50";
+            screenEl.addEventListener("click", evt => evt.stopPropagation());
+            screenEl.addEventListener("wheel", evt => editor.scrollDOM.scrollTop += evt.deltaY / 2);
+            editor.scrollDOM.appendChild(screenEl);
+        }
+
 
         // Set up scrolling callbacks
         scrollerEl.addEventListener("scroll", handleCheckScroll);
@@ -74,17 +87,28 @@
 </script>
 
 <div class =" h-[324px] flex flex-col">
-    <div class={"flex justify-between items-center bg-gray-50 h-[32px] " +
-        (shadowTop ? "shadow-md" : "")}
+    <div class="
+            flex justify-between items-center bg-gray-50 h-[32px]
+            {shadowTop ? "shadow-md" : ""}
+        "
     >
         <div>
-            <p class="text-xs text-gray-200 ps-2">Scripting Engine v0.0.1</p>
+            <p class="text-xs text-gray-200 ps-2">
+                Scripting Engine v0.0.1
+            </p>
         </div>
+
+        {#if editMode}
         <div class="flex items-center">
             <!-- Undo button -->
             <button
-                class={"block mr-3 text-xs border rounded-full px-2 border-gray-200 " +
-                    (undoDepth > 0 ? "text-gray-400 cursor-pointer" : "text-gray-100 cursor-default")}
+                class="
+                    block mr-3 text-xs border rounded-full px-2 border-gray-200
+                    {
+                        (undoDepth > 0) ? "text-gray-400 cursor-pointer" :
+                            "text-gray-100 cursor-default"
+                    }
+                "
                 on:click={() => editor.undo()}
             >
                 <Icon src={ArrowUturnLeft} size="14" />
@@ -92,27 +116,51 @@
 
             <!-- Redo button -->
             <button
-                class={"block mr-3 text-xs border-gray-200 border rounded-full px-2 " +
-                    (redoDepth > 0 ? "text-gray-400 cursor-pointer" : "text-gray-100 cursor-default")}
+                class="
+                    block mr-3 text-xs border-gray-200 border rounded-full px-2
+                    {
+                        (redoDepth > 0) ?
+                        "text-gray-400 cursor-pointer" :
+                        "text-gray-100 cursor-default"
+                    }
+                "
                 on:click={() => editor.redo()}
             >
                 <Icon src={ArrowUturnRight} size="14" />
             </button>
 
             <!-- Reload Script button -->
-            <button class="block mr-3 text-xs font-light text-gray-400 border-gray-200 border rounded-full px-2 cursor-pointer"
-                on:click={() => { if (doloadscript) doloadscript(); }}
+            <button class="
+                block mr-3 text-xs font-light text-gray-400
+                border-gray-200 border rounded-full px-2 cursor-pointer
+            "
+                on:click={() => doloadscript && doloadscript() }
             >
                 Reload Script
             </button>
         </div>
+        {:else}
+        <div class="flex items-center pe-2">
+            <Icon class="text-gray-200" src={LockClosed} solid size="16" />
+        </div>
+
+        {/if}
 
     </div>
 
 
+    <!-- Editor parent -->
+    <div bind:this={editorParentEl}
+        class="
+            flex-grow overflow-hidden relative
+            {editMode ? "" : "select-none"}
+        "
 
-    <div class="flex-grow overflow-hidden relative" bind:this={editorParentEl} >
-        <div class={"absolute w-full -bottom-2 h-2 bg-white rotate-180 " + (shadowBottom ? "shadow-md" : "")} />
+    >
+        <div class="
+            absolute w-full -bottom-2 h-2 bg-white rotate-180
+            {shadowBottom ? "shadow-md" : ""}
+        " />
     </div>
 </div>
 
